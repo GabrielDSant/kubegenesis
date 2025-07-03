b7856586c9500f0c7726da2e4ee4d9059d4ca333
GabrielDSant <gabrielbiel1597533@gmail.com>
2025-07-03 19:55:21 -0300

    adicionando estrutura do wsl

---
+++ kubegenesis/src/lib/utils.sh
@@ -0,0 +1,17 @@
+# kubegenesis/src/lib/utils.sh
+# Funções de log coloridas
+log_info() {
+    echo -e "\e[34m[INFO]\e[0m $1"
+}
+
+log_success() {
+    echo -e "\e[32m[SUCCESS]\e[0m $1"
+}
+
+log_warn() {
+    echo -e "\e[33m[WARN]\e[0m $1"
+}
+
+log_error() {
+    echo -e "\e[31m[ERROR]\e[0m $1"
+}
